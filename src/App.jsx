3547ade959cf021df1180b4c9a1a81563a6aee3e
Léo Canet <leo.canet@itsm-ng.com>
2025-01-07 15:02:28 +0100

    Add: side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { appsidebar } from "./components/appssidebar"
+
 function App() {
 
   return (
-    <div className="text-center text-white bg-blue-600 p-4">
-      <h1>Hello World</h1>
+    <div>
+      <appsidebar></appsidebar>
     </div>
   )
 }
