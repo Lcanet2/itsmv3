5cc9cf64a3f7ee6276f92a6350995622f3fd971e
Léo Canet <leo.canet@itsm-ng.com>
2025-02-06 14:34:49 +0100

    Init Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import Home from './pages/Home.tsx';
 
 
 function App() {
   return (
+    <div>
     <SidebarProvider>
       <AppSidebar />
       <main>
         <SidebarTrigger />
       </main>
     </SidebarProvider>
+    <Home />
+    </div>
   );
 }
 
